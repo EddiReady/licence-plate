87f8b8990252513dc73540cd9149451995d6f944
Eduards <114105915+Eddy-887@users.noreply.github.com>
2024-12-27 00:13:21 +0200

    Создал скелет программы, добавил функции, программа пока не работает

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 # Регистр LV номеров \\ by Eddy
+
+# В разработке: 
+# - Пользователь мог выбрать конкретные буквы или цифры, с которыми соответственно будут генерироваться цифры или буквы
+# - Добавление тех номеров в текстовый файл, которые пользователь захочет
+
 import random
 import string
+import os
+import time
 
-# Выбираются две случайные заглавные буквы
-random_letter1 = random.choice(string.ascii_uppercase)
-random_letter2 = random.choice(string.ascii_uppercase)
+def show_menu():
+    # Приветствие, начало программы
+    print("Добро пожаловать в программу \"Регистр LV номеров\"!")
+    time.sleep(2.5)
+    print("Здесь, с помощью рандома, вы можете выбить номер типа XX-1234 для своей тачки! (В разработке)")
+    time.sleep(2.5)
+    print("Номера, которые Вам понравятся, можно добавить в текстовый файл. (В разработке)")
+    time.sleep(2.5)
 
-# Выбираются рандомные цифры 
-a = str(random.randint(0, 9))
-b = str(random.randint(0, 9))
-c = str(random.randint(0, 9))
-d = str(random.randint(0, 9))
+def full_random():
+    pass
 
-# Циклы
-if a == "0":
-    a = " "
-if a == " " and b == "0":
-    b = " "
-if a == " " and b == " " and c == "0":
-    c = " "
-# XX-0110
-if a == " " and d == "0":
-    d = str(random.randint(0, 1))
-    if d == "1":
-        a = " "
-        d = " "
-# XX-0001 -> XX-0100
-if a == " " and b == " " and c == " ":
-    b = d
-    d = " "
-# XX-0011 -> XX-0110
-if a == " " and b == " ":
-    b = c
-    c = d
-    c = " "
-    d = " "
-# Вывод
-result = random_letter1 + random_letter2 + '-' + a + b + c + d
-print(result)
+def numbers_random():
+    pass
+
+def letters_random():
+    pass
+
+def main():
+    show_menu()
+    print()
+    print("Выберите желаемое действие:")
+    print("1. Рандомные номера")
+    print("2. Номер с конкретными буквами, цифры выбираются рандомные")
+    print("3. Номер с конкретными цифрами, буквы выбираются рандомные")
+    print("4. Выход")
+    time.sleep(1.5)
+    print()
+    choise = int(input("Введите цифру, которую выбрали (1-4): "))
+    print()
+    if choise == 1:
+        full_random()
+    elif choise == 2:
+        numbers_random()
+    elif choise == 3:
+        letters_random()
+    elif choise == 4:
+        print("Спасибо за использование программы! Надеюсь, она вам понравилась!")
+        time.sleep(0.5)
+        print("Буду рад вашему отзыву и предложениям по улучшению!")
+        time.sleep(0.5)
+        print("Связь со мной по почте: eduards.levsa@gmail.com")
+        input()
+        
+
+if __name__ == "__main__":
+    main()
